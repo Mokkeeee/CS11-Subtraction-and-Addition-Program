b3af790bc74d7972a133fd0152bb69ad6c06c188
Mokkeeee <evanthong11@gmail.com>
2025-12-01 07:03:22 +0000

    created addition and subtraction calculators

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+#Name: Evan Thong
+#Team Members: Jennifer Jiang and Chen-Ha Ma
+
+def addition():
+    num1 = input("What is the first number you want to add? ")
+    num2 = input("What is the second number you want to add? ")
+    ans = int(num1) + int(num2)
+    print(ans)
+
+def subtraction():
+    num_1 = input("What is the first number you want to subtract? ")
+    num_2 = input("What is the second number you want to subtract? ")
+    answers = int(num_1) - int(num_2)
+    print(answers)
+
+user_input = input("Do you want to do addition or subtraction: ")
+
+if user_input == "addition":
+    addition()
+
+elif user_input == "subtraction":
+    subtraction()
 
